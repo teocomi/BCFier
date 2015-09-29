e8630f18f8482f7b4ae2f231c183e9e7d2dae1ba
Matteo Cominetti <matteo@cominetti.org>
2015-09-29 18:28:45 -0300

    fixed assembly version

--- Bcfier.Win/Properties/AssemblyInfo.cs
+++ Bcfier.Win/Properties/AssemblyInfo.cs
@@ -49,6 +49,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.1")]
-[assembly: AssemblyFileVersion("1.0.1.1")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyFileVersion("2.0.1")]
 [assembly: GuidAttribute("9FC6BA85-A348-40B4-97C4-6699DA912C21")]
