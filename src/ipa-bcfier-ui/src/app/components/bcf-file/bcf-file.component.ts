ff3162b99c0b787f40c22cadf74cef761900c11c
Georg Dangl <georg@dangl.me>
2024-04-03 12:39:40 +0200

    Implement adding and removing topics

--- src/ipa-bcfier-ui/src/app/components/bcf-file/bcf-file.component.ts
+++ src/ipa-bcfier-ui/src/app/components/bcf-file/bcf-file.component.ts
@@ -8,6 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { TopicDetailComponent } from '../topic-detail/topic-detail.component';
 import { TopicFilterPipe } from '../../pipes/topic-filter.pipe';
 import { TopicPreviewImageDirective } from '../../directives/topic-preview-image.directive';
 
@@ -24,6 +25,7 @@ import { TopicPreviewImageDirective } from '../../directives/topic-preview-image
     FormsModule,
     TopicFilterPipe,
     MatProgressBarModule,
+    TopicDetailComponent,
   ],
   templateUrl: './bcf-file.component.html',
   styleUrl: './bcf-file.component.scss',
@@ -32,6 +34,10 @@ export class BcfFileComponent {
   @Input() bcfFile!: BcfFile;
 
   selectedTopic: BcfTopic | null = null;
+
+  ngOnInit() {
+    this.selectedTopic = this.bcfFile.topics[0] || null;
+  }
 
   private _search = '';
   public set search(value: string) {
@@ -43,5 +49,47 @@ export class BcfFileComponent {
 
   selectTopic(topic: BcfTopic) {
     this.selectedTopic = topic;
+  }
+
+  addIssue(): void {
+    const newIssue: BcfTopic = {
+      comments: [],
+      id: crypto.randomUUID(),
+      files: [],
+      labels: [],
+      referenceLinks: [],
+      documentReferences: [],
+      relatedTopicIds: [],
+      viewpoints: [],
+      assignedTo: '',
+      creationAuthor: '',
+      description: '',
+      priority: '',
+      title: 'New Issue',
+      topicStatus: '',
+      stage: '',
+      topicType: '',
+      serverAssignedId: '',
+      modifiedAuthor: '',
+      creationDate: new Date(),
+    };
+    this.bcfFile.topics.push(newIssue);
+    this.selectedTopic = newIssue;
+  }
+
+  removeIssue(): void {
+    if (!this.selectedTopic) {
+      return;
+    }
+
+    this.bcfFile.topics = this.bcfFile.topics.filter(
+      (topic) => topic.id !== this.selectedTopic?.id
+    );
+
+    if (this.bcfFile.topics.length > 0) {
+      this.selectedTopic = this.bcfFile.topics[0];
+    } else {
+      this.selectedTopic = null;
+    }
   }
 }
--- src/ipa-bcfier-ui/src/app/directives/topic-preview-image.directive.ts
+++ src/ipa-bcfier-ui/src/app/directives/topic-preview-image.directive.ts
@@ -1,22 +1,43 @@
 import { BcfFile, BcfTopic } from '../../generated/models';
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Directive({
   selector: '[bcfierTopicPreviewImage]',
   standalone: true,
 })
-export class TopicPreviewImageDirective implements OnInit {
+export class TopicPreviewImageDirective implements OnInit, OnChanges {
   @Input() bcfierTopicPreviewImage!: BcfTopic;
 
   constructor(private elementRef: ElementRef) {}
 
   ngOnInit(): void {
+    this.handleImage();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['bcfierTopicPreviewImage']) {
+      this.handleImage();
+    }
+  }
+
+  private handleImage(): void {
     const viewpoints = this.bcfierTopicPreviewImage.viewpoints.filter(
       (vp) => vp.snapshotBase64
     );
+
     if (viewpoints.length === 0) {
+      this.elementRef.nativeElement.style.display = 'none';
       return;
     }
+
+    this.elementRef.nativeElement.style.display = null;
 
     // We're using the base64 data from the snapshot to
     // generate a base64 data url with png type
