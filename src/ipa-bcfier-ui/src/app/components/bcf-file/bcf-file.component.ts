54ea717ba89184aeb65845e060c5725e6f65309d
Georg Dangl <georg@dangl.me>
2024-04-03 11:19:43 +0200

    Add topic selection to UI

--- src/ipa-bcfier-ui/src/app/components/bcf-file/bcf-file.component.ts
+++ src/ipa-bcfier-ui/src/app/components/bcf-file/bcf-file.component.ts
@@ -1,12 +1,13 @@
+import { BcfFile, BcfTopic } from '../../../generated/models';
 import { Component, Input } from '@angular/core';
 
-import { BcfFile } from '../../../generated/models';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { TopicFilterPipe } from '../../pipes/topic-filter.pipe';
 import { TopicPreviewImageDirective } from '../../directives/topic-preview-image.directive';
 
@@ -22,6 +23,7 @@ import { TopicPreviewImageDirective } from '../../directives/topic-preview-image
     TopicPreviewImageDirective,
     FormsModule,
     TopicFilterPipe,
+    MatProgressBarModule,
   ],
   templateUrl: './bcf-file.component.html',
   styleUrl: './bcf-file.component.scss',
@@ -29,11 +31,17 @@ import { TopicPreviewImageDirective } from '../../directives/topic-preview-image
 export class BcfFileComponent {
   @Input() bcfFile!: BcfFile;
 
+  selectedTopic: BcfTopic | null = null;
+
   private _search = '';
   public set search(value: string) {
     this._search = value;
   }
   public get search(): string {
     return this._search;
+  }
+
+  selectTopic(topic: BcfTopic) {
+    this.selectedTopic = topic;
   }
 }
