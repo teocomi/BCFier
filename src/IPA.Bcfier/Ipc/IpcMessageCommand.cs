cf05a8158f0b87ed716a35c830b4b2fa6a343084
Georg Dangl <georg@dangl.me>
2024-04-09 16:56:40 +0200

    Add IPC feature

---
+++ src/IPA.Bcfier/Ipc/IpcMessageCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace IPA.Bcfier.Ipc
+{
+    public enum IpcMessageCommand
+    {
+        AppClosed = 0,
+
+        CreateViewpoint = 1,
+
+        ViewpointCreated = 2,
+
+        ShowViewpoint = 3,
+
+        ViewpointShown = 4,
+    }
+}
